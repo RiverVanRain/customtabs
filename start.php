016f1db2503a014fdf6105fab9684f7d8d90bf30
RiverVanRain <river.vanrain@gmail.com>
2013-01-23 20:32:43 +0300

    Release initial

---
+++ start.php
@@ -0,0 +1,41 @@
+<?php
+
+	/**
+	 * Elgg Custom Tabs plugin
+	 *
+	 * @package Elgg Custom Tabs
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Alex Falk, RiverVanRain
+	 * @URL http://weborganizm.org/crewz/p/1983/personal-net
+	 * @copyright (c) weborganiZm 2k13
+	 */
+
+function customtabs_init() {
+global $CONFIG;
+elgg_register_page_handler('customtabs','customtabs_page_handler');
+				
+			// Add menu link
+$item = new ElggMenuItem('customtabs', elgg_echo('customtabs'), 'customtabs');
+elgg_register_menu_item('site', $item);
+				}
+
+
+
+function customtabs_page_handler($page) {
+	$base = elgg_get_plugins_path() . 'customtabs';
+
+	if (!isset($page[0])) {
+		$page[0] = 'tab1';
+	}
+
+	$vars = array();
+	$vars['page'] = $page[0];
+
+
+require_once "$base/index.php";
+	
+return true;
+}
+	
+elgg_register_event_handler('init', 'system', 'customtabs_init');
+?>
