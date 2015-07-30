a843d82cd286f9a12da17816384294ee3bc19449
RiverVanRain <river.vanrain@gmail.com>
2015-07-31 02:36:22 +0430

    Update ru.php

--- languages/ru.php
+++ languages/ru.php
@@ -5,8 +5,8 @@
 	 * @package Elgg Custom Tabs
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
 	 * @author Alex Falk, RiverVanRain
-	 * @URL http://weborganizm.org/crewz/p/1983/personal-net
-	 * @copyright (c) weborganiZm 2k13
+	 * @URL http://o.wzm.me/crewz/p/1983/personal-net
+	 * @copyright (c) organiZm 2k13
 	 */
 
 return array(
